27c59f9c141ec4d6ca57fe5592d44df9d46029dd
anas1k <anass.elk.48@gmail.com>
2022-09-12 02:18:25 +0100

    12 & 13

--- week_01/Challenge12.c
+++ week_01/Challenge12.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 	// varible declaration
-		
+	int a, b, c;
+	printf("Saisir les trois numbres \n");
+	printf("Saisir le 1er numbre : ");
+	scanf("%d", &a);
+	printf("Saisir le 2eme numbre : ");
+	scanf("%d", &b);
+	printf("Saisir le 3eme numbre : ");
+	scanf("%d", &c);
+	
+	printf("les trois saisie sont : %d %d %d, l'inverse : %d %d %d",a,b,c, c, b, a);	
 	return 0;
 }
---
+++ week_01/Challenge13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// varible declaration
+	int a;
+	printf("Saisir votre nombre : ");
+	scanf("%d",&a);
+	
+	printf("la valeur en octal : %o , en hexadecimal : %x",a,a);
+	return 0;
+}
