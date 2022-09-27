b6be7502f18c672c64ddc3203daec36981e18928
anas1k <anass.elk.48@gmail.com>
2022-09-27 15:46:39 +0100

    v0.5

---
+++ challenge_sas/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float* meanModeFunction(int n, float arr[]){
+    float moyenne ;
+    for (int i =0 ; i<n ;i++){
+        moyenne += arr[i];
+    }
+    moyenne /= n;
+    premier = arr[0];
+    for(int i =0; i<n;i++){
+        if(arr[n]== moyenne){
+
+        }
+    }
+
+}
+
+int main()
+{
+    float arr[]
+
+    return 0;
+}
